62ea33555a6f32c0c60a14b1a07c11e25ed49fd4
Evan <evantyoverdy95@gmail.com>
2019-07-10 18:54:50 +0700

    latihan

--- world.py
+++ world.py
@@ -1,71 +1,39 @@
+import sys
 import pydot
 
-asia = [
-    {
-        "Japan": [],
-        "Korea": [],
-        "India": [],
-        "Indonesia": [],
-    }
-]
-
-europe = [
-    {
-        "France": [],
-        "Germany": [],
-        "Italy": [],
-        "Netherlands": [],
-    }
-]
-
-africa = [
-    {
-        "Kenya": [],
-        "South Africa": [],
-        "Uganda": [],
-        "Rwanda": [],
-    }
-]
-
-america = [
-    {
-        "Canada": [],
-        "Brazil": [],
-        "Chile": [],
-        "United States": [],
-    }
-]
-
-australia = [
-    {
-    "Australia": [],
-    }
-]
-
-continent = [{
-    "Continent": [{
-        "Asia": asia,
-        "Europe": europe,
-        "Africa": africa,
-        "America": america,
-        "Australia": australia,
-    }],
-}]
-
-
-def iterate(course_list, parent=None, filename=None):
-    for c in course_list:
-        for key, val in c.items():
-            if parent is not None:
-                graph.add_edge(pydot.Edge(parent, key))
-            if val is not None or len(val) > 0:
-                iterate(val, parent=key)
-
-
-
+# graph = pydot.Dot(graph_type='digraph',, size="1920,1080" )
 graph = pydot.Dot(graph_type='digraph', rankdir='LR')
-iterate(continent, parent=None, filename='world_continent.png')
 
+root = 'Curriculum'
+days = ['Hari 1', 'Hari 2', 'Hari 3', 'Hari 4', 'Hari 5', 'Hari 6']
+for day in days:
+    graph.add_edge(pydot.Edge(root, day))
 
+day1 = ["Basic Python Syntaxes",
+        "Modularization - Function",
+        "Modularization - Class",
+        "Modularization - Package",
+        "Modularization - Study Case - Pydot Usage"]
 
+for d1 in day1:
+    graph.add_edge(pydot.Edge(days[0], d1))
 
+day1_basic = [
+    "Sequential Code",
+    "Branching with IF",
+    "Looping with For and While"
+]
+
+for day2 in day1_basic:
+    graph.add_edge(pydot.Edge(day1[0], day2))
+
+day1_class = [
+    "Encapsulation",
+    "Inheritance",
+    "Polymorphism"
+]
+
+for d2c in day1_class:
+    graph.add_edge(pydot.Edge(day1[2], d2c))
+
+graph.write_png('curriculum.png')
